03d04ddb89df381e3ecb302adf34742d97b0281d
Bryan Goss <bcgoss@gmail.com>
2016-07-16 17:31:28 -0600

    started working on ships

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,8 +1,19 @@
 require './test/test_helper'
 require './lib/ship'
-
+require './lib/location'
 class ShipTest < Minitest::Test
   def test_it_exists
-    assert Ship.new
+    assert Ship.new(2)
+  end
+
+  def test_ships_have_sizes
+    submarine = Ship.new(2)
+
+    assert_equal 2, submarine.size
+  end
+
+  def test_ships_have_locations
+    start = Location
+    submarine = Ship.new()
   end
 end
