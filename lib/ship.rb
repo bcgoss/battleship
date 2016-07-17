05ca02c0534589027ed5340926a288a83c92c7a6
Bryan Goss <bcgoss@gmail.com>
2016-07-16 19:51:11 -0600

    Ships are working but they don't know any rules.

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,21 @@
 class Ship
-  attr_reader :size
+  attr_reader :size,
+              :locations,
+              :hits
 
-  def initialize(size)
-    @size = size
+  def initialize(starting, ending)
+    @locations = assign_locations(starting, ending)
+    @size = locations.count
+    @hits = 0
+  end
+
+  def assign_locations(starting, ending)
+    return [starting, ending]
+  end
+
+  def hit
+    @hits += 1
+
+    size > hits ? hits : "sunk"
   end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,18 +2,25 @@ require './test/test_helper'
 require './lib/ship'
 require './lib/location'
 class ShipTest < Minitest::Test
-  def test_it_exists
-    assert Ship.new(2)
-  end
-
-  def test_ships_have_sizes
-    submarine = Ship.new(2)
-
-    assert_equal 2, submarine.size
-  end
-
   def test_ships_have_locations
-    start = Location
-    submarine = Ship.new()
+    start = "a1"
+    ending = "a2"
+    submarine = Ship.new(start, ending)
+    assert_equal [start, ending], submarine.locations
+  end
+
+  def test_ships_dont_know_the_rules
+    submarine = Ship.new("a1","b2")
+    assert_equal ["a1","b2"], submarine.locations
+  end
+
+  def test_ships_count_hits
+    submarine = Ship.new("","")
+    assert_equal 1, submarine.hit
+  end
+  def test_ships_sink_after_too_many_hits
+    submarine = Ship.new('','')
+    submarine.hit
+    assert_equal "sunk", submarine.hit
   end
 end
